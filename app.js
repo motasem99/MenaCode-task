c7f327b3c83b81568c467800201f42f817077610
mutasem <motasemimad10@gmail.com>
2023-03-14 14:56:58 +0200

    build second Owl-Caasourel

--- app.js
+++ app.js
@@ -19,3 +19,27 @@ $('.owl-carousel').owlCarousel({
     },
   },
 });
+
+$('.owl-carousel.second').owlCarousel({
+  // loop: true,
+  margin: 10,
+  // autoplay: true,
+  nav: true,
+  navText: [
+    "<div class='nav-btn prev-slide'></div>",
+    "<div class='nav-btn next-slide'></div>",
+  ],
+  // autoplayTimeout: 2000,
+  // autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
